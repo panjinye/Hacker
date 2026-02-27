21b60e6f3b90bc0e4232c88460e0299866481793
panjinye <panjinye@gmail.com>
2026-02-27 17:33:58 +0800

    颜色切换待上线

--- dist/_astro/hoisted.CliaoSgh.js
+++
@@ -1,1 +0,0 @@
-const t=document.getElementById("theme-toggle"),e=document.body;localStorage.getItem("theme")==="dark"||!localStorage.getItem("theme")&&window.matchMedia("(prefers-color-scheme: dark)").matches?e.classList.add("dark"):e.classList.remove("dark");t.addEventListener("click",()=>{e.classList.toggle("dark"),localStorage.setItem("theme",e.classList.contains("dark")?"dark":"light")});
